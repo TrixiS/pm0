43d7661fe7705f1fd10e62cd53e63a7ad58f7719
TrixiS <oficialmorozov@gmail.com>
2024-04-11 16:37:46 +0300

    Refactor cli code x2

--- internal/cli/commands/list.go
+++ internal/cli/commands/list.go
@@ -1,9 +1,7 @@
 package commands
 
 import (
-	"fmt"
 	"os"
-	"time"
 
 	pm0 "github.com/TrixiS/pm0/internal/cli"
 	"github.com/TrixiS/pm0/internal/cli/command_context"
@@ -12,47 +10,6 @@ import (
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/jedib0t/go-pretty/v6/text"
 )
-
-var runningStatusString = text.FgGreen.Sprint("Running")
-var exitedStatusString = text.FgWhite.Sprint("Exited")
-var failedStatusString = text.FgRed.Sprint("Failed")
-var stoppedStatusString = text.FgYellow.Sprint("Stopped")
-
-const tableNoneString = "None"
-
-func formatNillablePointer[T any](ptr *T) string {
-	if ptr == nil {
-		return tableNoneString
-	}
-
-	return fmt.Sprintf("%v", *ptr)
-}
-
-func getUnitUptime(startedAt int64, status daemon.UnitStatus) string {
-	if status != daemon.RUNNING {
-		return tableNoneString
-	}
-
-	startedAtTime := time.Unix(startedAt, 0)
-	now := time.Now()
-	diff := now.Sub(startedAtTime)
-	return diff.Round(time.Second).String()
-}
-
-func formatUnitStatus(unitStatus daemon.UnitStatus) string {
-	switch unitStatus {
-	case daemon.RUNNING:
-		return runningStatusString
-	case daemon.EXITED:
-		return exitedStatusString
-	case daemon.FAILED:
-		return failedStatusString
-	case daemon.STOPPED:
-		return stoppedStatusString
-	default:
-		return "Unknown"
-	}
-}
 
 func List(ctx *command_context.CommandContext) error {
 	return ctx.Provider.WithClient(func(client pb.ProcessServiceClient) error {
@@ -85,10 +42,10 @@ func List(ctx *command_context.CommandContext) error {
 			t.AppendRow(table.Row{
 				unit.Id,
 				unit.Name,
-				formatNillablePointer(unit.Pid),
-				formatUnitStatus(unitStatus),
+				pm0.FormatNillablePointer(unit.Pid),
+				pm0.FormatUnitStatus(unitStatus),
 				unit.RestartsCount,
-				getUnitUptime(unit.StartedAt, unitStatus),
+				pm0.FormatUnitStartedAt(unit.StartedAt, unitStatus),
 			})
 		}
 
