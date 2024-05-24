e8a0060265ed458b646fb0563eb86b1e009e1913
TrixiS <oficialmorozov@gmail.com>
2024-05-24 08:09:51 +0300

    Make start command use cwd base path if no name is set

--- internal/cli/commands/start.go
+++ internal/cli/commands/start.go
@@ -3,12 +3,14 @@ package commands
 import (
 	"fmt"
 	"os"
+	"path"
 
 	pm0 "github.com/TrixiS/pm0/internal/cli"
 	"github.com/TrixiS/pm0/internal/cli/command_context"
 	"github.com/TrixiS/pm0/internal/daemon/pb"
 )
 
+// TODO: if name is empty string (make it optional) then decide name from cwd dirname
 func Start(ctx *command_context.CommandContext) error {
 	if ctx.CLIContext.NArg() == 0 {
 		return fmt.Errorf("specify a binary and optional args")
@@ -20,11 +22,17 @@ func Start(ctx *command_context.CommandContext) error {
 		return err
 	}
 
+	name := ctx.CLIContext.String("name")
+
+	if len(name) == 0 {
+		name = path.Base(cwd)
+	}
+
 	bin := ctx.CLIContext.Args().First()
 	args := ctx.CLIContext.Args().Tail()
 
 	request := pb.StartRequest{
-		Name: ctx.CLIContext.String("name"),
+		Name: name,
 		Bin:  bin,
 		Args: args,
 		Cwd:  cwd,
@@ -37,7 +45,7 @@ func Start(ctx *command_context.CommandContext) error {
 			return err
 		}
 
-		pm0.Printf("started unit with id %d and PID %d", response.Unit.Id, *response.Unit.Pid)
+		pm0.Printf("started unit %s with id %d and PID %d", name, response.Unit.Id, *response.Unit.Pid)
 		return nil
 	})
 }
